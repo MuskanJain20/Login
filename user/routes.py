c62043f7d0f5ff56f48d3cfb279d8c221074a5e4
simran <simransetia11111@gmail.com>
2022-04-18 12:00:12 +0530

    second commit

---
+++ user/routes.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+from app import app
+from user.models import User
+
+@app.route('/user/signup', methods=['POST'])
+def signup():
+  return User().signup()
+
+@app.route('/user/signout')
+def signout():
+  return User().signout()
+@app.route("/index/")
+def index():
+    return render_template('index.html')
+
+
+@app.route('/user/login', methods=['POST'])
+def login():
+  return User().login()
+app.run()
